be072a076ab94d8676782945b0e3ecda2c0cc049
Dodul01 <mhdodul299@gmail.com>
2024-04-23 23:58:43 +0600

    firebase auth file created

---
+++ client/src/Context/AuthProviders.jsx
@@ -0,0 +1,15 @@
+import { getAuth } from 'firebase/auth';
+import { createContext } from 'react';
+
+export const AuthContext = createContext(null);
+
+const AuthProviders = () => {
+    
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default AuthProviders
