4c459534051671bf90b506a32d39c115465852cc
Dodul01 <mhdodul299@gmail.com>
2024-05-01 21:12:53 +0600

    Printing the cart item

--- client/src/Components/Card/Card.jsx
+++ client/src/Components/Card/Card.jsx
@@ -1,6 +1,10 @@
 const Card = ({ item }) => {
     const { image, name, recipe, price } = item;
 
+    const getItem = (food) => {
+        console.log(food);
+    }
+
     return (
         <div className="max-w-sm bg-[#E8E8E8] border border-gray-200 rounded-lg shadow">
             <span className="relative">
@@ -13,7 +17,7 @@ const Card = ({ item }) => {
                 </span>
                 <p className="mb-3 font-normal text-[#737373]">{recipe}</p>
                 <div className="flex items-center justify-center">
-                    <button className="px-4 py-3 text-sm transition-all font-medium text-center border border-b-2 border-b-[#BB8506] text-white bg-[#BB8506] rounded-lg hover:bg-[#111827] hover:text-[#BB8506] focus:ring-4 focus:outline-none focus:ring-[#bb85063d]">
+                    <button onClick={() => getItem(item)} className="px-4 py-3 text-sm transition-all font-medium text-center border border-b-2 border-b-[#BB8506] text-white bg-[#BB8506] rounded-lg hover:bg-[#111827] hover:text-[#BB8506] focus:ring-4 focus:outline-none focus:ring-[#bb85063d]">
                         ADD TO CART
                     </button>
                 </div>
