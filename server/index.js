c57df083e24480441c01da359c8ddbee09434107
Dodul01 <mhdodul299@gmail.com>
2024-07-05 00:10:08 +0600

    delete user , make admin api created

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -48,6 +49,27 @@ async function run() {
 
         app.get('/users', async (req, res) => {
             const result = await usersCollection.find().toArray();
+            res.send(result);
+        })
+
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await usersCollection.deleteOne(query);
+
+            res.send(result);
+        })
+
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = {_id: new ObjectId(id)};
+            const updatedUser = {
+                $set : {
+                    role: 'admin'
+                }
+            }
+            const result = await usersCollection.updateOne(filter, updatedUser);
+
             res.send(result);
         })
 
